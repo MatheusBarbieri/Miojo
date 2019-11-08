e0b9b100ce78a56dfa2d6f5eff823052c087cfe0
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-08 20:15:31 -0300

    feat: create and use numpy array

--- src/neural-network.py
+++ src/neural-network.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def generate_random_weights(layers):
-    return [np.random.rand(input_num, layer_size + 1) for layer_size, input_num in zip(layers, layers[1:])]
+    return np.array([np.random.rand(input_num, layer_size + 1) for layer_size, input_num in zip(layers, layers[1:])])
 
 
 def add_bias(instance):
@@ -13,10 +13,14 @@ def sigmoid(z):
     return 1 / (1 + np.exp(-z))
 
 
+def loss_function():
+    pass
+
+
 class NeuralNetwork:
     def __init__(self, layers=[5, 5, 5, 2], weights=None):
         self.layers = layers
-        self.weights = weights or generate_random_weights(layers)
+        self.weights = weights if weights is not None else generate_random_weights(layers)
 
     def train(self, batches_size=10):
         pass
