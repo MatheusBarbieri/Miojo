9834117e29b3280d4635a3943fb344153b0b881e
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-13 22:12:49 -0300

    feat: add backpropagation validation mode

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
 from src.command_line import get_args
+from src.neural_network import NeuralNetwork
+from src.file_system import load_dataset_from_text, load_network_from_text, load_weights_from_text
+from src.numeric_validation import BackpropagationValidator
+
+
+def main():
+    args = get_args()
+
+    mode = args.mode
+
+    if mode == 'backpropagation':
+        regularization, layers = load_network_from_text(args.network_path)
+        examples, results = load_dataset_from_text(args.dataset_path)
+        weights = load_weights_from_text(args.weights_path)
+
+        neural_network = NeuralNetwork(layers=layers, weights=weights, regularization_factor=regularization)
+        bp_validator = BackpropagationValidator(neural_network, examples, results)
+        bp_validator.show_gradients()
 
 
 if __name__ == "__main__":
-    args = get_args()
+    main()
