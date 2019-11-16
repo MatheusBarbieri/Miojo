85e4db6169f46cfdee4917cefad730f3b054399c
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-15 21:23:48 -0300

    refactor: wrap split generation in a class

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -2,41 +2,46 @@ import pandas as pd
 from numpy.random import randint
 
 
-def _random_k_folds(df, k, add_remaining, seed):
-    fold_size = len(df) // k
+class KFolds:
+    def __init__(self, data, k_folds=5, sampling='stratified'):
+        self._data = data
+        self._num_folds = k_folds
+        self._sampling = sampling
 
-    folds = []
-    for i in range(k):
-        sample = df.sample(n=fold_size, random_state=seed)
-        df = df.drop(sample.index, errors='ignore')
-        folds.append(sample)
+    def _random_k_folds(self, add_remaining, seed):
+        df = self._data
+        fold_size = len(df) // self._num_folds
 
-    if add_remaining:
-        for i in range(len(df)):
-            folds[i] = pd.concat([folds[i], df.iloc[i:i+1]])
+        folds = []
+        for i in range(self._num_folds):
+            sample = df.sample(n=fold_size, random_state=seed)
+            df = df.drop(sample.index, errors='ignore')
+            folds.append(sample)
 
-    return folds
+        if add_remaining:
+            for i in range(len(df)):
+                folds[i] = pd.concat([folds[i], df.iloc[i:i+1]])
 
+        return folds
 
-def _stratified_k_folds(df, k, add_remaining, seed):
-    groups = df.groupby('class')
-    folds_by_groups = [_random_k_folds(g, k, add_remaining, seed) for c, g in groups]
-    folds = [pd.concat(folds_by_groups[x][y] for x in range(len(folds_by_groups))) for y in range(k)]
-    return folds
+    def _stratified_k_folds(self, add_remaining, seed):
+        groups = self.data.groupby('class')
+        folds_by_groups = [self._random_k_folds(g, add_remaining, seed) for c, g in groups]
+        folds = [pd.concat(folds_by_groups[x][y] for x in range(len(folds_by_groups))) for y in range(self._num_folds)]
+        return folds
 
+    def _k_folds(self, add_remaining=True, seed=randint(10000)):
+        if self._sampling == 'random':
+            return self._random_k_folds(add_remaining, seed)
+        elif self._sampling == 'stratified':
+            return self._stratified_k_folds(add_remaining, seed)
+        else:
+            raise Exception("Sampling parameter must be one of [stratified, random]")
 
-def k_folds(df, k, sampling='stratified', add_remaining=True, seed=randint(10000)):
-    if sampling == 'random':
-        return _random_k_folds(df, k, add_remaining, seed)
-    elif sampling == 'stratified':
-        return _stratified_k_folds(df, k, add_remaining, seed)
-    else:
-        raise Exception("Sampling parameter must be one of [stratified, random]")
+    def _splits(self, folds):
+        for i, fold in enumerate(folds):
+            yield pd.concat(folds[:i] + folds[i + 1:]), folds[i]
 
-
-def splits(folds):
-    sets = []
-    for i, fold in enumerate(folds):
-        sets.append((pd.concat(folds[:i] + folds[i + 1:]), folds[i]))
-
-    return sets
+    def split_generator(self):
+        folds = self._k_folds()
+        return self._splits(folds)
