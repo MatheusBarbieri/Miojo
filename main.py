099277e740d2b56b8f15abbf5f2fa33342065cf3
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-15 23:27:04 -0300

    feat: train

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import pandas as pd
+
 from src.command_line import get_args
-from src.file_system import load_model_from_text, load_dataset_from_text
+from src.file_system import load_model_from_text, load_dataset_from_text, load_dataset
 from src.numeric_validation import BackpropagationValidator, GradientNumericValidator
+from src.util import normalize_dataset
+from src.neural_network import NeuralNetwork
 
 
 def main():
@@ -16,6 +20,26 @@ def main():
         ValidatorClass = GradientNumericValidator if mode == 'gradient' else BackpropagationValidator
         validator = ValidatorClass(neural_network, examples, results)
         validator.show(verbose=verbose)
+
+    elif mode == 'train':
+        data = load_dataset(args.dataset_path)
+        normalized_data = normalize_dataset(data)
+        examples = normalized_data.drop(['class'], axis=1)
+        expected = pd.get_dummies(normalized_data['class'])
+        num_inputs = len(examples.columns)
+        num_outputs = len(expected.columns)
+        layers = [num_inputs] + args.structure + [num_outputs]
+
+        neural_network = NeuralNetwork(
+            layers=layers,
+            regularization_factor=args.regularization,
+            learning_rate=args.learning_rate,
+            batch_size=args.batch_size,
+            epochs=args.epochs,
+            show_loss=verbose)
+
+        neural_network.train(examples.values, expected.values)
+        neural_network.save(args.output_path)
 
 
 if __name__ == "__main__":
