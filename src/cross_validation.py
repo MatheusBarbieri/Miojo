6e8747f7e53e9cbc352bbaf69709b6ebb6f58deb
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-17 21:33:23 -0300

    feat: cross validation now reset index of train and test splits

--- src/cross_validation.py
+++ src/cross_validation.py
@@ -8,8 +8,8 @@ class KFolds:
         self._num_folds = k_folds
         self._sampling = sampling
 
-    def _random_k_folds(self, add_remaining, seed):
-        df = self._data
+    def _random_k_folds(self, data, add_remaining, seed):
+        df = data
         fold_size = len(df) // self._num_folds
 
         folds = []
@@ -25,14 +25,14 @@ class KFolds:
         return folds
 
     def _stratified_k_folds(self, add_remaining, seed):
-        groups = self.data.groupby('class')
+        groups = self._data.groupby('class')
         folds_by_groups = [self._random_k_folds(g, add_remaining, seed) for c, g in groups]
         folds = [pd.concat(folds_by_groups[x][y] for x in range(len(folds_by_groups))) for y in range(self._num_folds)]
         return folds
 
     def _k_folds(self, add_remaining=True, seed=randint(10000)):
         if self._sampling == 'random':
-            return self._random_k_folds(add_remaining, seed)
+            return self._random_k_folds(self.data, add_remaining, seed)
         elif self._sampling == 'stratified':
             return self._stratified_k_folds(add_remaining, seed)
         else:
@@ -40,7 +40,9 @@ class KFolds:
 
     def _splits(self, folds):
         for i, fold in enumerate(folds):
-            yield pd.concat(folds[:i] + folds[i + 1:]), folds[i]
+            train = pd.concat(folds[:i] + folds[i + 1:]).reset_index(drop=True)
+            test = folds[i].reset_index(drop=True)
+            yield train, test
 
     def split_generator(self):
         folds = self._k_folds()
