e070dc0ea95e277adfe67ac81a3ac0fc652de428
matheus.barbieri <matheus.barbieri@corp.globo.com>
2019-11-17 21:36:22 -0300

    feat: add model validation procedure to main

--- main.py
+++ main.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
 from src.command_line import get_args
+from src.cross_validation import KFolds
 from src.file_system import load_model_from_text, load_dataset_from_text, load_dataset
+from src.metrics import ConfusionMatrix
+from src.neural_network import NeuralNetwork
 from src.numeric_validation import BackpropagationValidator, GradientNumericValidator
 from src.util import (
-    normalize_dataset,
-    get_attributes,
     attributes_and_target,
-    results_to_labels,
-    expected_to_neural_network
+    expected_to_neural_network,
+    generate_structure,
+    get_attributes,
+    normalize_dataset,
+    results_to_labels
 )
-from src.neural_network import NeuralNetwork
 
 
 def main():
@@ -21,10 +24,10 @@ def main():
 
     if mode in ['backpropagation', 'gradient']:
         neural_network = load_model_from_text(args.network_path, args.weights_path)
-        examples, results = load_dataset_from_text(args.dataset_path)
+        examples, test_results = load_dataset_from_text(args.dataset_path)
 
         ValidatorClass = GradientNumericValidator if mode == 'gradient' else BackpropagationValidator
-        validator = ValidatorClass(neural_network, examples, results)
+        validator = ValidatorClass(neural_network, examples, test_results)
         validator.show(verbose=verbose)
 
     elif mode == 'train':
@@ -53,10 +56,48 @@ def main():
         attributes, expected, expected_columns = attributes_and_target(normalized_data)
 
         neural_network = NeuralNetwork.load(args.model_path)
-        results = neural_network.predict(attributes.values)
+        test_results = neural_network.predict(attributes.values)
 
-        results_df = results_to_labels(results, expected_columns).join(expected)
+        results_df = results_to_labels(test_results, expected_columns).join(expected)
         results_df.to_csv(args.results_path, index=False)
+
+    elif mode == 'validate':
+        data = load_dataset(args.dataset_path)
+        normalized_data = normalize_dataset(data)
+
+        k_folds = KFolds(normalized_data, args.k_folds, sampling='stratified')
+        splits = k_folds.split_generator()
+
+        all_results = []
+        for train, test in splits:
+            train_attributes, train_expected, train_columns = attributes_and_target(train)
+            test_attributes, test_expected, test_columns = attributes_and_target(test)
+
+            train_expected = expected_to_neural_network(train_expected)
+
+            layers = generate_structure(train_attributes, train_expected, args.structure)
+
+            neural_network = NeuralNetwork(
+                layers=layers,
+                regularization_factor=args.regularization,
+                learning_rate=args.learning_rate,
+                batch_size=args.batch_size,
+                epochs=args.epochs,
+                show_loss=verbose
+            )
+
+            neural_network.train(train_attributes.values, train_expected.values)
+            test_results = neural_network.predict(test_attributes.values)
+            results = results_to_labels(test_results, test_columns).join(test_expected)
+            all_results.append(results)
+            break
+
+        confusion_matrix = ConfusionMatrix(pd.concat(all_results))
+        confusion_matrix.show()
+
+    else:
+        raise Exception('No valid execution mode was found. \
+            Should be one of [Backpropagation, gradient, train, predict, validate]')
 
 
 if __name__ == "__main__":
